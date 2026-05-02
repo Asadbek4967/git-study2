d8dca9db167bf9d65be4f099530d14f2eaef7676
Asadbek Matyaqubov <matyaqubovasadbek8@gmail.com>
2026-05-02 16:06:55 +0900

    add new line in main

--- src/main/java/git_study2/project/ProjectApplication.java
+++ src/main/java/git_study2/project/ProjectApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectApplication {
-
 	public static void main(String[] args) {
+		System.out.println("new line");
+
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
